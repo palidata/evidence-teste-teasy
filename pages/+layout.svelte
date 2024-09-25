1245832f588e49fa1006d8442746ef686056949c
Eric Yang <ericyaang@gmail.com>
2024-09-25 10:16:34 -0300

    custom layout

---
+++ pages/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data}
+neverShowQueries=true
+hideSidebar=true
+hideTOC=true
+hideHeader=true
+>
+	<slot slot="content" />
+</EvidenceDefaultLayout>
